bf5d22fa93687d4e83f1491b67fb82f2e50df3ae
GhostAkr <ighost99@gmail.com>
2022-08-04 16:16:38 +0300

    [+] Template factorial computator
    - Added TemplateFactorial computator class
    - Fixed naming in recursive factorial computator

--- template_factorial/template_factorial.hpp
+++ template_factorial/template_factorial.hpp
@@ -3,4 +3,38 @@
 
 int recursiveFactorial(int n);
 
+template<int n>
+class TemplateFactorial
+{
+public:
+    int _value;
+
+public:
+    TemplateFactorial() : 
+        _value(n * TemplateFactorial<n - 1>().value())
+    {}
+
+    int value() const
+    {
+        return _value;
+    }
+};
+
+template<>
+class TemplateFactorial<0>
+{
+public:
+    int _value;
+
+public:
+    TemplateFactorial() : 
+        _value(1)
+    {}
+
+    int value() const
+    {
+        return _value;
+    }
+};
+
 #endif  // _TEMPLATE_FACTORIAL_HPP_
--- template_factorial/tests.cpp
+++ template_factorial/tests.cpp
@@ -2,8 +2,10 @@
 
 #include "template_factorial.hpp"
 
-void testRecursiveTemplate()
+void testRecursiveFactorial()
 {
+    std::cout << "Recursive factorial tests:" << std::endl;
+
     int n1 = -1, n2 = 0, n3 = 1, n4 = 2, n5 = 5, n6 = 6;
 
     int r1 = recursiveFactorial(n1);
@@ -25,7 +27,33 @@ void testRecursiveTemplate()
     std::cout << n6 << "! = " << r6 << std::endl;
 }
 
+void testTemplateFactorial()
+{
+    std::cout << "Template factorial tests:" << std::endl;
+
+    const int n1 = -1, n2 = 0, n3 = 1, n4 = 2, n5 = 5, n6 = 6;
+
+    // int r1 = TemplateFactorial<n1>().value();
+    // std::cout << n1 << "! = " << r1 << std::endl;
+
+    int r2 = TemplateFactorial<n2>().value();
+    std::cout << n2 << "! = " << r2 << std::endl;
+
+    int r3 = TemplateFactorial<n3>().value();
+    std::cout << n3 << "! = " << r3 << std::endl;
+
+    int r4 = TemplateFactorial<n4>().value();
+    std::cout << n4 << "! = " << r4 << std::endl;
+
+    int r5 = TemplateFactorial<n5>().value();
+    std::cout << n5 << "! = " << r5 << std::endl;
+
+    int r6 = TemplateFactorial<n6>().value();
+    std::cout << n6 << "! = " << r6 << std::endl;
+}
+
 int main()
 {
-    testRecursiveTemplate();
+    testRecursiveFactorial();
+    testTemplateFactorial();
 }
