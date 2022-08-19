44d7bb6c0fd3490e494e99fffa64d60174258acc
Alexander Antoshkin <ighost99@gmail.com>
2022-08-19 17:06:47 +0300

    [+] SudokuValidator implementation - Added initial copy/move ctors and dtor - Added isValidSol() method - Added example in main()

--- sudoku_validator/include/SudokuValidator.hpp
+++ sudoku_validator/include/SudokuValidator.hpp
@@ -1,4 +1,28 @@
 #ifndef SUDOKUVALIDATOR_HPP
 #define SUDOKUVALIDATOR_HPP
 
+#include <vector>
+
+using uint = unsigned int;
+
+class SudokuValidator
+{
+public:
+    SudokuValidator();
+    explicit SudokuValidator(const std::vector<std::vector<uint>>& initList);
+    ~SudokuValidator() = default;
+
+    SudokuValidator(const SudokuValidator& other) = default;
+    SudokuValidator(SudokuValidator&& other) = default;
+
+    SudokuValidator& operator=(const SudokuValidator& other) = default;
+    SudokuValidator& operator=(SudokuValidator&& other) = default;
+
+    bool isValidSol() const;
+
+private:
+    const size_t _fieldSize;
+    std::vector<std::vector<uint>> _field;
+};
+
 #endif
--- sudoku_validator/src/SudokuValidator.cpp
+++ sudoku_validator/src/SudokuValidator.cpp
@@ -1,2 +1,47 @@
-#include <iostream>
 #include "../include/SudokuValidator.hpp"
+
+SudokuValidator::SudokuValidator()
+: _fieldSize(9)
+, _field()
+{}
+
+SudokuValidator::SudokuValidator(const std::vector<std::vector<uint>>& initList)
+: _fieldSize(9)
+, _field(initList)
+{}
+
+bool SudokuValidator::isValidSol() const
+{
+    std::vector<int> rowSum(_fieldSize, 0);
+    std::vector<int> colSum(_fieldSize, 0);
+    std::vector<int> sqSum(_fieldSize, 0);
+
+    for (size_t rowIx = 0; rowIx < _field.size(); ++rowIx)
+    {
+        for (size_t colIx = 0; colIx < _field[rowIx].size(); ++colIx)
+        {
+            uint val = _field[rowIx][colIx];
+            rowSum[rowIx] += val;
+            colSum[colIx] += val;
+
+            const size_t sqInRow = 3;
+            const size_t sqSize = 3;
+            const size_t squareIx = (rowIx / sqSize) * sqInRow + (colIx / sqSize);
+            sqSum[squareIx] += val;
+        }
+    }
+
+    const int targetSum = 45;
+    for (size_t sumIx = 0; sumIx < _fieldSize; ++sumIx)
+    {
+        if (rowSum[sumIx] != targetSum 
+            || colSum[sumIx] != targetSum 
+            || sqSum[sumIx] != targetSum
+        )
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
